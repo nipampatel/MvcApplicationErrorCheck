acdee53fd93b6ed123ba82ad7a67e114f0cf41ed
Nipam Patel <nipam2000@gmail.com>
2013-11-27 16:41:22 -0500

    First commit

---
+++ MvcApplicationErrorCheck/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplicationErrorCheck.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Error404()        
+        {
+            Response.StatusCode = 404;
+            return View();
+        }
+
+        public ActionResult Error403()
+        {
+            Response.StatusCode = 403;
+            return View();
+        }
+    }
+}
---
+++ MvcApplicationErrorCheck/Handlers/ImageHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplicationErrorCheck.Handlers
+{
+    public class ImageHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            throw new HttpException(403, "Throw 404");
+        }
+
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+    }
+}
